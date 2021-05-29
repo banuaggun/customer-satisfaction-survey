88f01ea1f420b723b2c4671b9b09cf5b85da11e0
Banu AĞGÜN <37499235+banuaggun@users.noreply.github.com>
2021-05-29 16:52:33 +0300

    comments deleted

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+var currentTab = 0; 
+showTab(currentTab); 
 function showTab(n) {
-  // This function will display the specified tab of the form...
   var x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
-  //... and fix the Previous/Next buttons:
   if (n == 0) {
     document.getElementById("prevBtn").style.display = "none";
   } else {
@@ -15,29 +13,21 @@ function showTab(n) {
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
-  //... and run a function that will display the correct step indicator:
 }
 
 function nextPrev(n) {
-  // This function will figure out which tab to display
   var x = document.getElementsByClassName("tab");
 
-  // Exit the function if any field in the current tab is invalid:
   if (n == 0) return false;
-  // Hide the current tab:
   x[currentTab].style.display = "none";
-  // Increase or decrease the current tab by 1:
   currentTab = currentTab + n;
-  // if you have reached the end of the form...
   if (currentTab >= x.length) {
-    // ... the form gets submitted:
     document.getElementById("regForm").submit();
   
 
    
     return false;
   }
-  // Otherwise, display the correct tab:
   showTab(currentTab);
 
   
